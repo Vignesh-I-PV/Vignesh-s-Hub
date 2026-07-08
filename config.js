16fb0502212f7df0f44f86a3aa9934a9bcfb04a3
Vignesh-I-PV <vignesh.iyer@meesho.com>
2026-07-08 17:45:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'https://vignesh-i-pv.github.io/Vignesh-s-Hub/',
+  url: 'https://jblqzdjyrsgxuibjlmgk.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpibHF6ZGp5cnNneHVpYmpsbWdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MDYwODYsImV4cCI6MjA5OTA4MjA4Nn0.v0VQCo1f3ZbIXFmS-LxWQlQbDA4Uzz1tPsG_FdDfobI'
 };
